43e39e3f64ec4ebe1ee90cf560b483e339ec0715
dbranquinho <delermando@gmail.com>
2017-09-18 19:31:27 -0300

    new features

--- MyWordCloud/ui.R
+++ MyWordCloud/ui.R
@@ -3,13 +3,13 @@ shinyUI(fluidPage(
   titlePanel("Criando minha Nuvem de Palavras"),
   sidebarLayout(
     sidebarPanel(
-            fileInput("file1", "Arquivo Excel", multiple = FALSE),
-            actionButton("update", "Reorganizar"),
             tags$div(class="header", checked=NA,
-                     tags$br(),tags$br(),tags$br(),
-                     tags$p("Delermando Branquinho Filho"),
                      tags$img(src = "http://www.thescientist.com.br/images/logo2t.gif", 
-                              width = "130px", height = "100px"))
+                              width = "130px", height = "100px")),
+            tags$p("Delermando Branquinho Filho"),
+            tags$hr(),
+            fileInput("file1", "Arquivo Excel", multiple = FALSE),
+            actionButton("update", "Reorganizar")
             ),
     
     # Show a plot of the generated distribution
@@ -24,7 +24,7 @@ shinyUI(fluidPage(
                         tags$p("3. Na segunda coluna você pode inserir valores para dizer o tamanho das palavras"),
                         tags$p("4. Salve em uma pasta e faça o upload aqui."),
                         tags$p("5. Clique em atualizar para mudar a nuvem"),
-                        tags$p("4. Clique com o botão direito do mouse >> salvar imagem como"),
+                        tags$p("6. Clique com o botão direito do mouse >> salvar imagem como"),
                         tags$a(href="http://www.delermando.com.br/portfolio", "The Scientist")
                )
        )
